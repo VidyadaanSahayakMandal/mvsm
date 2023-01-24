8e56a86f98a63ea3220e2efd69f72bffe886666e
Pushkar Saraf <pushkar.saraf.ossu@gmail.com>
2023-01-24 23:51:14 +0530

    Missing files

---
+++ Useful Commands.md
@@ -0,0 +1,3 @@
+## Useful Commands
+flutter pub run build_runner watch
+to generate boilerplate code if using functional flutter lib
---
+++ lib/widgets/goal.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'goal.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class Goal extends StatelessWidget {
+  const Goal({
+    Key? key,
+    required this.color,
+    required this.width,
+    required this.icon,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
+
+  final MaterialColor color;
+
+  final double width;
+
+  final IconData icon;
+
+  final String title;
+
+  final String description;
+
+  @override
+  Widget build(BuildContext _context) => goal(
+        _context,
+        color: color,
+        width: width,
+        icon: icon,
+        title: title,
+        description: description,
+      );
+}
